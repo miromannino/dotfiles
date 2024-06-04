e56e5916d75055dd40f9cb12e6b24d601139bd31
Miro Mannino <miro.mannino@gmail.com>
2024-06-04 21:26:36 +0400

    windows style alt-tab implementation

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -14,3 +14,14 @@ spoon.MiroWindowsManager:bindHotkeys({
   move        = {hyper, "v"},
   middle      = {hyper, "m"}
 })
+
+-- Windows style ALT-TAB
+switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{})
+hs.hotkey.bind('alt', 'tab', function() switcher:next() end)
+hs.hotkey.bind('alt-shift', 'tab', function() switcher:previous() end)
+hs.hotkey.bind('alt', 'tab', hs.window.switcher.nextWindow)
+hs.hotkey.bind('alt-shift', 'tab', hs.window.switcher.previousWindow, nil, hs.window.switcher.previousWindow)
+hs.window.switcher.ui.showSelectedTitle = false
+hs.window.switcher.ui.showSelectedThumbnail = false
+hs.window.switcher.ui.thumbnailSize = 128
+hs.window.animationDuration = 0
